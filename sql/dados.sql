cf45324821e059cc6b8b615c0fd509c4b6c3a40e
Flavio Andrade <foa12381@gmail.com>
2023-07-10 12:44:23 -0300

    sql queries

--- sql/dados.sql
+++ sql/dados.sql
@@ -26,11 +26,11 @@ CREATE TABLE aluno_curso (
 
 
 INSERT INTO aluno (primeiro_nome, ultimo_nome, data_nascimento) VALUES (
-	'Vinicius', 'Dias', '1997-10-15'
+	'Carla', 'Dias', '1997-10-15'
 ), (
 	'Patricia', 'Freitas', '1986-10-25'
 ), (
-	'Diogo', 'Oliveira', '1984-08-27'
+	'Mara', 'Oliveira', '1984-08-27'
 ), (
 	'Maria', 'Rosa', '1985-01-01'
 );

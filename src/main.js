33372c62956826042174ce631575ff9dbcd602fa
Flavio Andrade <foa12381@gmail.com>
2023-07-11 10:35:15 -0300

    querie copy

--- src/main.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  var dialog = document.getElementById('myFirstDialog');
-  document.getElementById('show').onclick = function() {
-      dialog.show();
-  };
-  document.getElementById('hide').onclick = function() {
-      dialog.close();
-  };
-})();

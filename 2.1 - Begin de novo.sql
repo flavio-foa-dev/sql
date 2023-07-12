457849d589b68c44e5caa7aedc51b20cd506d0f7
Flavio Andrade <foa12381@gmail.com>
2023-07-12 14:26:27 -0300

    querie function plpgsql trigger

---
+++ 2.1 - Begin de novo.sql
@@ -0,0 +1,32 @@
+CREATE FUNCTION cria_instrutor() RETURNS void AS $$ 
+    DECLARE
+        media_salarial DECIMAL;
+        instrutor_recebe_menos INTEGER DEFAULT 0;
+        total_instrutores INTEGER DEFAULT 0;
+        salario DECIMAL;
+        percentual DECIMAL(5,2);
+    BEGIN
+        SELECT AVG(instrutor.salario) INTO media_salarial FROM instrutor WHERE id <> NEW.id;
+
+        BEGIN;
+        IF NEW.salario > media_salarial THEN
+            INSERT INTO log_instrutores (informacao) VALUES (NEW.nome || ' recebe acima da média');
+        END IF;
+
+        FOR salario IN SELECT instrutor.salario FROM instrutor WHERE id <> NEW.id LOOP
+            total_instrutores := total_instrutores + 1;
+
+            IF NEW.salario > salario THEN
+                instrutor_recebe_menos := instrutor_recebe_menos + 1;
+            END IF;    
+        END LOOP;
+
+        percentual = instrutor_recebe_menos::DECIMAL / total_instrutores::DECIMAL * 100;
+
+        INSERT INTO log_instrutores (informacao) 
+            VALUES (NEW.nome || ' recebe mais do que ' || percentual || '% da grade de instrutores');
+        COMMIT;
+
+        RETURN NEW;    
+    END;
+$$ LANGUAGE plpgsql;
